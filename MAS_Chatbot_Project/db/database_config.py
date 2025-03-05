5c671288a11ffcc209c9938d5bdaa285e4048d57
NirvanaSKh <sanchit.khandelwal@gmail.com>
2025-03-05 14:18:08 +0000

    Update database_config.py

--- MAS_Chatbot_Project/db/database_config.py
+++ MAS_Chatbot_Project/db/database_config.py
@@ -1,6 +1,8 @@
 DB_CONFIG = {
-    "dbname": "your_db_name",
-    "user": "your_user",
-    "password": "your_password",
-    "host": "your_host"
+    "dbname": "neondb",  # Database name
+    "user": "neondb_owner",  # Username
+    "password": "npg_68rBGRMzfIFv",  # Password
+    "host": "ep-quiet-mountain-a9z5li2u-pooler.gwc.azure.neon.tech",  # Host
+    "port": "5432",  # PostgreSQL default port
+    "sslmode": "require"  # Ensure SSL connection
 }
