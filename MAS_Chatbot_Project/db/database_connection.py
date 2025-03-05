1b3a1bd89eb71317f243f8794ef450691649ae9f
NirvanaSKh <sanchit.khandelwal@gmail.com>
2025-03-05 14:18:26 +0000

    Update database_connection.py

--- MAS_Chatbot_Project/db/database_connection.py
+++ MAS_Chatbot_Project/db/database_connection.py
@@ -2,5 +2,10 @@ import psycopg2
 from .database_config import DB_CONFIG
 
 def connect_to_db():
-    connection = psycopg2.connect(**DB_CONFIG)
-    return connection
+    try:
+        # Establish the connection using the database config
+        connection = psycopg2.connect(**DB_CONFIG)
+        return connection
+    except Exception as e:
+        print(f"Error: Unable to connect to the database: {e}")
+        return None
